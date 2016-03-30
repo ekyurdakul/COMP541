b28b0c38e537ce59ad69b8953a745270e3cc713b
Ekrem Emre Yurdakul <eeyrdkl@gmail.com>
2016-03-30 11:14:52 +0300

    New code

--- Model.jl
+++ Model.jl
@@ -1,8 +1,10 @@
 using Knet
+
 #Dropout layer
 @knet function dropout(x; pdrop=0.5)
 	return x .* rnd(init=Bernoulli(1-pdrop, 1/(1-pdrop)))
 end
+
 #Produces the top 4096 dimensional vector
 @knet function Layer3D(x)
 	w=par(init=Gaussian(0.0, 0.01), dims=(5,5,5,3,96))
@@ -30,10 +32,12 @@ end
 	y=dropout(y)
 	return y
 end
-#TBD: Produces the bottom 4096 dimensional vector
+
+#TBD: ImageNet Produces the bottom 4096 dimensional vector
 @knet function Layer2D(x)
 	return x
 end
+
 #Class prediction
 @knet function FCClass(x)
 	w=par(init=Gaussian(0.0, 0.01), dims=(20,0))
@@ -42,7 +46,8 @@ end
 	y=soft(y)
 	return y
 end
-#Box prediction
+
+#TBD: Box prediction
 @knet function FCBox(x)
 	w=par(init=Gaussian(0.0, 0.01), dims=(120,0))
 	b=par(init=Constant(0.0), dims=(120,1))
@@ -51,12 +56,6 @@ end
 	return y
 end
 
-
-
-
-
-
-
 #Entire model
 @knet function Model(x2d, x3d)
 	#Each produce 4096 dimensional vector
@@ -64,27 +63,55 @@ end
 	v2=Layer2D(x2d)
 
 	#Reduce 8192 dimensions to 1000
-	w=par(init=Gaussian(0.0, 0.01), dims=(1000,0))
-	b=par(init=Constant(0.0), dims=(1000,1))
-	y=w*x.+b
-	y=relu(y)
-	y=dropout(y)
-	v=y
+	w1=par(init=Gaussian(0.0, 0.01), dims=(1000, 0))
+	w2=par(init=Gaussian(0.0, 0.01), dims=(1000, 0))
+	b1=par(init=Constant(0.0), dims=(1000,1))
+	b2=par(init=Constant(0.0), dims=(1000,1))
+
+	#Simulate concatenation
+	y1=w1*v1.+b1
+	y2=w2*v2.+b2
+	v=y1+y2
+
+	v=relu(v)
+	v=dropout(v)
 	
 	#Feed v into 2 different layers, class and box prediction
 	pClass=FCClass(v)
 	pBox=FCBox(v)
 end
+model=compile(:Model)
 
 
 
-batchsize=7
-x3d=zeros(30,30,30,3,batchsize)
-x2d=zeros(4096,batchsize)
 
 
 
-#model=compile(:Model)
-#forw(model, x2d, x3d)
 
+
+
+
+
+
+
+
+
+
+
+#Debug function
+function printModel()
+	batchsize=7;
+	x3d=zeros(30,30,30,3,batchsize);
+	x2d=zeros(4096,batchsize);
+
+	forw(model, x2d, x3d)
+
+	println("Model structure:")
+	Knet.netprint(model)
+	println()
+
+	println("Output sizes: (Batchsize=$batchsize)")
+	println("Class: $(size(get(model, :pClass)))")
+	println("Box:   $(size(get(model, :pBox)))")
+end
 
