a70c62a0a2ebe673584b30139eb27a5ceeefb75f
Ekrem Emre Yurdakul <eeyrdkl@gmail.com>
2016-04-04 13:41:58 +0300

    Basics

---
+++ src/Basics.jl
@@ -0,0 +1,37 @@
+using Knet
+
+#Dropout layer
+@knet function dropout(x; pdrop=0.5)
+	return x .* rnd(init=Bernoulli(1-pdrop, 1/(1-pdrop)))
+end
+
+#Save trained model weights to file
+function saveTrainedModel(model, filename)
+	JLD.save(filename, "model", clean(model))
+end
+
+#Load trained model
+function loadTrainedModel(filename)
+	return JLD.load(filename, "model")
+end
+
+
+
+
+
+
+
+
+
+
+
+#TBD: L1 Smooth layer
+@knet function L1Smooth(x)
+#=
+	if (abs(x) < 1)
+		y= 0.5*x*x;
+	else
+		y=abs(x)-0.5
+	end
+=#
+end
